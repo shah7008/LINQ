c2380cc9f91830670fefd52c6e6a23b536f9c9bc
shah7008 <shah7008@gmail.com>
2020-04-30 00:32:04 +0500

    Add project files.

---
+++ ProjectLINQ/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectLINQ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBt_Click(object sender, RoutedEventArgs e)
+        {
+            string query=searchTb.Text;
+            //now find query in file 1 and textblock 1 populates with matched line 
+            //After first the 2 and 3 textblocks populates with files 2 and 3 having alternates of textblock 1
+
+            String strPath = @"C:\jimmybuffett";
+            var songs = from song in Directory.GetFiles(strPath, "*", SearchOption.AllDirectories)
+                        where File.ReadAllLines(song).Contains(query)
+                        select song;
+
+            foreach (var song in songs)
+            {
+                textBlock1.Text = textBlock1.Text + song;
+            }
+        }
+    }
+}
